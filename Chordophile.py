0b51ff9bef05d1ed5279717308621314f2806737
Bryan <brbangs@yahoo.com>
2015-07-03 22:16:38 -0400

    Now works with sharps

--- Chordophile.py
+++ Chordophile.py
@@ -1,12 +1,10 @@
 ##### Make a dictionary with notes corresponding to pitches (inputs for MIDIutil)
-##### 
 
 #################Hey Joe! This program lets you find the 'note numbers' for any defined chord in any key! Try typing something like:
 
 #chor=chord()
 #chor.finder('insertnoteheremajt') for example chor.finder('amajt'). You can also change the  chord type, e.g. chor.finder('amaj7m')
 # You should get the 'numbers' as an output.
-# Do I really have to write 'self' before EVERYTHING like I've been doing, though?
 
 
 
@@ -14,21 +12,7 @@ class chord:
     
     def __init__(self):
         
-        
-        self.chords = {
-        'majt':[0,4,7], 
-        'mint':[0,3,7],
-        'majaug':[0,4,8],
-        'majdim':[0,4,6],
-        'mindim':[0,3,6],
-        'maj7':[0,4,7,10],
-        'maj7m':[0,4,7,11],
-        'min7':[0,3,7,10],
-        'min7m':[0,3,7,11],
-        'maj6':[0,4,7,9],
-        'min6':[0,3,7,9]}
-        
-        self.notes = {
+         self.notes = {
         'a':0,
         'asharp':1,
         'b':2,
@@ -42,35 +26,49 @@ class chord:
         'g':10,
         'gsharp':11}
         
-    def finder(self,chord):
+         self.chords = {
+        'majt':[0,4,7], 
+        'mint':[0,3,7],
+        'majaug':[0,4,8],
+        'majdim':[0,4,6],
+        'mindim':[0,3,6],
+        'maj7':[0,4,7,10],
+        'maj7m':[0,4,7,11],
+        'min7':[0,3,7,10],
+        'min7m':[0,3,7,11],
+        'maj6':[0,4,7,9],
+        'min6':[0,3,7,9]}
         
-        self.chord = chord
+       
         
-        self.ckey = (str(self.chord))[0]     
-                                                # Gets the first letter of the chord, the keynote ('X'majt)
+    def finder(self,chord):  
+        ########################################FINDER FINDS THE NOTES OF ANY TYPE OF CHORD IN ANY KEY. 
         
-        self.ctype = (str(self.chord))[1:]       
-                                                # Gets the chord type, such as 'majt'.
+        ########################################The following if/else block gets the keynote of the chord. ckey=chord keynote.
+        if(chord[1] == 's'):
+            ckey = chord[0:6] #############WHY???
+            ctype = chord[6:]                                         
         
-        self.steps = self.notes[self.ckey]
+        else:  
+            ckey = chord[0]
+            ctype = chord[1:] 
+        
+        ######################################## Set up some more variables:
+        
+        steps = self.notes[ckey]
                                                 # Finds the 'number' of the keynote by looking it up in the note-number dictionary.
-        
-        self.chordnotes = self.chords[self.ctype]
+        chordnotes = self.chords[ctype]
                                                 # Looks up chord type in dictionary, gets list of numbers of each chord note.
-        
-        self.chordout = []
+        chordout = []
         ######################################### The following FOR loop adds the Key note number to each of the chords numbers, thus shifting it up (eventually down):
-        for self.note in self.chordnotes:
+        for note in chordnotes:
             
-             self.note = self.note + self.steps
+             note = note + steps
              
-             self.chordout.append(self.note)
+             chordout.append(note)
              
-        return self.chordout
+        return chordout
         
-    
-    
-    ####################### THERE IS ONE BIG FLAW: IT CAN'T DEAL WITH SHARPS YET. I'll figure that out tomorrow. :)
              
                
         
