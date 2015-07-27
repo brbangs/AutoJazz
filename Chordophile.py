272ead67c3c98bec98603884e5cd8c3ea6b1114d
Bryan <brbangs@yahoo.com>
2015-07-26 22:02:40 -0400

    Added notefinder to Chordophile

--- Chordophile.py
+++ Chordophile.py
@@ -7,7 +7,80 @@
 # You should get the 'numbers' as an output.
 
 
+class noteswehg:
+    def __init__(self):
+        
+        self.notes = {'a':0,
+        'b':2,
+        'c':3,
+        'd':5,
+        'e':7,
+        'f':8,
+        'g':10}
+     
+        
+        
+    def noteget(self,note):
+        tone = note[0]
+        basicNote = tone.lower()                    #Gets first (lower case) letter (generalized note) of note name.
+        basicNumber =  self.notes[basicNote]    #Gets the note number corresponding with the middle-C octave note.
+        
+        ###############The following "If/Else" block determines whether the note should be made lower or higher using Case of beginning letter.
+        
+        if(tone.isupper()):
+            octShift = -12
+            downShift = -12
+        else:
+            octShift = 12
+            downShift = 0
+        #The following "while loop" moves the note the proper number of octaves up or down, depending on the number of key letters.
+        octave = 0
+        index = 1
+        while((index < len(note)) and (note[index] == note[index-1])):
+                
+                octave = octave + octShift
+                
+                index = index + 1
+        
+        #### The following system figures out the value for the accidental.
+        #This figures out whether there is any accidental at all:
+        try:
+            accident = note[index]
+        except:
+            accident = 0
+        
+        #This figures out what the accidental's value should be:
+        if (accident == 's'):
+            accidental = 1
+        elif (accident == 'f'):
+            accidental = -1
+        elif (accident == 'l'):
+            accidental = -13                 # This is to deal with 'fflat'.
+        else:
+            accidental = 0
+        
+        # Finally, all of the elements are added together:
+        
+        notenumber = basicNumber + octave + downShift + accidental
+        
+        return notenumber
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################USE TWO INSTANCES: NOTE = CHORD(), CHORD = CHORD(). RENAME THE CLASS TO SOMETHING MORE GENERAL.
+########################THAT WAY, CAN USE FINDER AND OTHER FUNCTIONS ON NOTES AND CHORDS.
 class chord:
     
     def __init__(self):
@@ -33,27 +106,10 @@ class chord:
         'min7m':[0,3,7,11],
         'maj6':[0,4,7,9],
         'min6':[0,3,7,9]}
-    
-    def accidental(self,chord):
-        
-        self.chord = chord
-        
-        if (self.chord[1] == 's'):
-            
-            self.steps = self.steps + 1
-            self.ckey = self.chord[1:6]
-            self.ctype = self.chord[6:]
-        
-        elif (self.chord[1] == 'f'):
-            
-            self.steps = self.steps - 1
             
 
         
-            
-       
-        
-    def finder(self,chord):  
+    def chordget(self,chord):  
         ########################################FINDER FINDS THE NOTES OF ANY TYPE OF CHORD IN ANY KEY. 
         
         ########################################The following if/else block gets the keynote of the chord.
